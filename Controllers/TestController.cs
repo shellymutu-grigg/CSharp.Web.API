3583d8c351bf87fda693322bb1670ddbf7e5106a
Shelly Mutu-Grigg <shelly.mutu-grigg@serko.com>
2022-09-23 09:18:44 +1200

    Start parameter validation

---
+++ Controllers/TestController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CSharp.Web.API.Controllers;
+
+[Route("api/blah")]
+[ApiController]
+public class TestController : ControllerBase
+{
+	[HttpGet]
+	public string GetEndpoint()
+	{
+		return "Hello World";
+	}
+
+	[HttpPost]
+	public string PostEndpoint()
+	{
+		return "complete";
+	}
+
+	[HttpGet]
+	[Route("funny")]
+	public string BeFunny()
+	{
+		return "Fun and games";
+	}
+
+	[HttpGet("anotherfunny")]
+	public string AnotherFunny()
+	{
+		return "Fun and games 02";
+	}
+}
